cce0acfd48890debe63d33f5a8062bc5bcfb80ad
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2026-01-08 11:13:16 -0300

    register.html

---
+++ scripts/tailwindcss/config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  darkMode: 'media',
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+    },
+    extend: {
+      colors: {
+        background: {
+          light: '#ffffff',
+          dark: '#0f172a',
+        },
+        surface: {
+          light: '#f8fafc',
+          dark: '#020617',
+        }
+      }
+    }
+  }
+}
